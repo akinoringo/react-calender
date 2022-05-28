f1f392a41438466dd30a6e8b3e5363bf42fb3939
akinoringo <wbraver@outlook.jp>
2022-05-28 14:09:14 +0900

    feat: クリック時した日付を取得

--- src/components/organisms/Calendar.tsx
+++ src/components/organisms/Calendar.tsx
@@ -2,25 +2,31 @@ import * as React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import jaLocale from '@fullcalendar/core/locales/ja';
-
-const AddEvent = () => {
-  const today = new Date()
-  return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
-    2,
-    '0'
-  )}`
-}
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction"
 
 export const Calendar = () => {
+  const addEvent = () => {
+    const today = new Date()
+    return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
+      2,
+      '0'
+    )}`
+  }
+
+  const handleDateClick = React.useCallback((arg: DateClickArg) => {
+    alert(arg.dateStr)
+  },[])
+
   return (
     <FullCalendar
-      plugins={[dayGridPlugin]}
+      plugins={[dayGridPlugin, interactionPlugin]}
       initialView="dayGridMonth"
       locale={jaLocale}
       events={[
-        { title: "event 1", date: `${AddEvent()}-01` },
-        { title: "event 2", date: `${AddEvent()}-02` },
+        { title: "event 1", date: `${addEvent()}-01` },
+        { title: "event 2", date: `${addEvent()}-02` },
       ]}
+      dateClick={handleDateClick}
     />
   )
 }

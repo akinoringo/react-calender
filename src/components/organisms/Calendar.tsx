1305c29c3925143bd74f82be3bc6af70a073d025
akinoringo <wbraver@outlook.jp>
2022-05-28 14:00:06 +0900

    feat: イベントの追加

--- src/components/organisms/Calendar.tsx
+++ src/components/organisms/Calendar.tsx
@@ -3,12 +3,24 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import jaLocale from '@fullcalendar/core/locales/ja';
 
+const AddEvent = () => {
+  const today = new Date()
+  return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
+    2,
+    '0'
+  )}`
+}
+
 export const Calendar = () => {
   return (
     <FullCalendar
       plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
       locale={jaLocale}
+      events={[
+        { title: "event 1", date: `${AddEvent()}-01` },
+        { title: "event 2", date: `${AddEvent()}-02` },
+      ]}
     />
   )
 }

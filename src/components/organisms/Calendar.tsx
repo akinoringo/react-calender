29924a9e63539f2b915c4af02c6452f6cd3f2e5d
akinoringo <wbraver@outlook.jp>
2022-05-28 13:51:59 +0900

    feat: 今日が英語のtodayとなる不具合への対応(@fullcalendar/coreの導入)

--- src/components/organisms/Calendar.tsx
+++ src/components/organisms/Calendar.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import jaLocale from '@fullcalendar/core/locales/ja';
 
 export const Calendar = () => {
-  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" locale="ja" />
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      locale={jaLocale}
+    />
+  )
 }
